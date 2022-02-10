6a0f62600325f4d61c090aa05db018dace1f8f63
Madhav Kothandaraman <44755991+LightWarpMedia@users.noreply.github.com>
2022-02-11 00:00:23 +0530

    GUI updates

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -19,15 +19,15 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
     int line1 = 0;
     int line2 = 13;
     int line3 = 26;
-
+    int line4 = 39;
     float fontFactor            = 1.0;
 
     // define text to place
     QString titleText       = QString(QApplication::applicationName()).replace(QString("-testnet"), QString(""), Qt::CaseSensitive); // cut of testnet, place it as single object further down
     QString versionText     = QString("Version %1 ").arg(QString::fromStdString(FormatFullVersion()));
     QString copyrightText1   = QChar(0xA9)+QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Bitcoin developers"));
-    QString copyrightText2   = QChar(0xA9)+QString(" 2011-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Trihardcoin developers"));
-
+    QString copyrightText2   = QChar(0xA9)+QString(" 2011-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Litecoin developers"));
+    QString copyrightText3   = QChar(0xA9)+QString(" %1 ").arg(2017) + QString(tr("Trihardcoin Developer"));
     QString font            = "Arial";
 
     // load the bitmap for writing some text over it
